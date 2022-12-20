5f63930890351f9dbb0f2391139daa5e4478d01b
Omar Abdelkhalek <omarmedhat099@gmail.com>
2022-12-20 23:50:51 +0200

    add clustering

---
+++ lib/data/water_well_data.dart
@@ -0,0 +1,82 @@
+import 'dart:js';
+import 'dart:ui';
+
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:clippy_flutter/triangle.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:google_maps_cluster_manager/google_maps_cluster_manager.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class WaterWellData with ClusterItem {
+  WaterWellData({
+    required this.name,
+    required this.information,
+    required this.latitude,
+    required this.longitude,
+    required this.thumbnailImage,
+    this.extraImages,
+  });
+  final String name;
+  final String information;
+  final double latitude;
+  final double longitude;
+  final CachedNetworkImage thumbnailImage;
+  final List<CachedNetworkImage>? extraImages;
+
+  Marker toMarker() => Marker(
+        markerId: MarkerId(name),
+        position: LatLng(latitude, longitude),
+        icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueOrange),
+        infoWindow: InfoWindow(
+          title: name,
+          snippet: information,
+        ),
+      );
+  LatLng getPosition() => LatLng(latitude, longitude);
+
+  @override
+  LatLng get location => LatLng(latitude, longitude);
+
+  /*Widget getCustomInfoWindowWidget() => Column(
+        children: [
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+                borderRadius: BorderRadius.circular(4),
+              ),
+              width: double.infinity,
+              height: double.infinity,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.account_circle,
+                      color: Colors.white,
+                      size: 30,
+                    ),
+                    SizedBox(
+                      width: 8.0,
+                    ),
+                    Text(
+                      "I am here",
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Triangle.isosceles(
+            edge: Edge.BOTTOM,
+            child: Container(
+              color: Colors.blue,
+              width: 20.0,
+              height: 10.0,
+            ),
+          ),
+        ],
+      );*/
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
+import 'dart:async';
+import 'dart:ui';
+
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:google_maps_cluster_manager/google_maps_cluster_manager.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:google_maps_flutter_web/google_maps_flutter_web.dart';
+import 'package:water_wells_web/data/water_well_data.dart';
 
 void main() {
   runApp(const MyApp());
@@ -39,16 +45,41 @@ class _MyHomePageState extends State<MyHomePage> {
     zoom: 10,
   );
   Set<Marker> markers = {};
+  List<WaterWellData> waterWells = [
+    WaterWellData(
+      name: 'water well 1',
+      information: 'test information\nmore test information',
+      latitude: 31,
+      longitude: 31,
+      thumbnailImage: CachedNetworkImage(
+        imageUrl: 'https://www.care.org.tr/files/2017/04/img_0225-jpg.jpg',
+        progressIndicatorBuilder: (context, url, downloadProgress) =>
+            CircularProgressIndicator(value: downloadProgress.progress),
+        errorWidget: (context, url, error) => const Icon(Icons.error),
+      ),
+    ),
+  ];
+  late ClusterManager _manager;
+  Completer<GoogleMapController> _controller = Completer();
+  late GoogleMapController _googleMapController;
+  List<WaterWellData> items = [
+    for (int i = 0; i < 100; i++)
+      WaterWellData(
+        name: 'Place $i',
+        latitude: 48.848200 + i * 0.001,
+        longitude: 2.319124 + i * 0.001,
+        thumbnailImage: CachedNetworkImage(
+          imageUrl: '',
+        ),
+        information: 'Place $i',
+      ),
+  ];
   Future<void> setMarkers() async {
     markers.add(
       Marker(
-        markerId: MarkerId('0'),
-        position: LatLng(31, 31),
-        icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueOrange),
-        infoWindow: InfoWindow(
-          title: 'Test place',
-          snippet: 'test test test',
-        ),
+        markerId: MarkerId(waterWells[0].name),
+        position: waterWells[0].getPosition(),
+        onTap: () {},
       ),
     );
     markers.add(
@@ -65,27 +96,106 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
     );
-    markers.add(
-      Marker(
-        markerId: MarkerId('2'),
-        position: LatLng(31.02, 31.02),
-        /*icon: await BitmapDescriptor.fromAssetImage(
-          const ImageConfiguration(),
-          'images/bg-avatar.png',
-        ),*/
-        infoWindow: InfoWindow(
-          title: 'Test place',
-          snippet: 'test test test',
-        ),
-      ),
+  }
+
+  void _updateMarkers(Set<Marker> markers) {
+    print('Updated ${markers.length} markers');
+    setState(() {
+      this.markers = markers;
+    });
+  }
+
+  Future<Marker> Function(Cluster<WaterWellData>) get _markerBuilder =>
+      (cluster) async {
+        return Marker(
+          markerId: MarkerId(cluster.getId()),
+          position: cluster.location,
+          infoWindow: !cluster.isMultiple
+              ? InfoWindow(
+                  title: cluster.items.first.name,
+                  snippet:
+                      '${cluster.items.first.information}\nTap here to view more information!',
+                )
+              : const InfoWindow(),
+          onTap: () async {
+            print('---- $cluster');
+            cluster.items.forEach((p) => print(p));
+            if (cluster.isMultiple) {
+              var currentZoomLevel = await _googleMapController.getZoomLevel();
+
+              currentZoomLevel = currentZoomLevel + 2;
+              _googleMapController.animateCamera(
+                CameraUpdate.newCameraPosition(
+                  CameraPosition(
+                    target: cluster.location,
+                    zoom: currentZoomLevel,
+                  ),
+                ),
+              );
+            }
+          },
+          icon: await _getMarkerBitmap(
+            cluster.isMultiple ? 125 : 75,
+            text: cluster.isMultiple ? cluster.count.toString() : null,
+          ),
+        );
+      };
+  Future<BitmapDescriptor> _getMarkerBitmap(int size, {String? text}) async {
+    if (kIsWeb) size = (size / 2).floor();
+
+    final PictureRecorder pictureRecorder = PictureRecorder();
+    final Canvas canvas = Canvas(pictureRecorder);
+    final Paint paint1 = Paint()..color = Colors.orange;
+    final Paint paint2 = Paint()..color = Colors.white;
+
+    canvas.drawCircle(Offset(size / 2, size / 2), size / 2.0, paint1);
+    canvas.drawCircle(Offset(size / 2, size / 2), size / 2.2, paint2);
+    canvas.drawCircle(Offset(size / 2, size / 2), size / 2.8, paint1);
+
+    if (text != null) {
+      TextPainter painter = TextPainter(textDirection: TextDirection.ltr);
+      painter.text = TextSpan(
+        text: text,
+        style: TextStyle(
+            fontSize: size / 3,
+            color: Colors.white,
+            fontWeight: FontWeight.normal),
+      );
+      painter.layout();
+      painter.paint(
+        canvas,
+        Offset(size / 2 - painter.width / 2, size / 2 - painter.height / 2),
+      );
+    }
+
+    final img = await pictureRecorder.endRecording().toImage(size, size);
+    final data = await img.toByteData(format: ImageByteFormat.png) as ByteData;
+
+    return BitmapDescriptor.fromBytes(data.buffer.asUint8List());
+  }
+
+  ClusterManager _initClusterManager() {
+    return ClusterManager<WaterWellData>(
+      items,
+      _updateMarkers,
+      markerBuilder: _markerBuilder,
     );
   }
 
   @override
   void initState() {
-    setMarkers();
+    //setMarkers();
+    _manager = _initClusterManager();
     super.initState();
   }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  final CameraPosition _parisCameraPosition =
+      const CameraPosition(target: LatLng(48.856613, 2.352222), zoom: 12.0);
 
   @override
   Widget build(BuildContext context) {
@@ -96,10 +206,18 @@ class _MyHomePageState extends State<MyHomePage> {
         body: Stack(
           children: [
             GoogleMap(
-              initialCameraPosition: _defaultLocation,
+              initialCameraPosition: _parisCameraPosition,
               minMaxZoomPreference: const MinMaxZoomPreference(4, 100000),
               rotateGesturesEnabled: false,
               markers: markers,
+              onTap: (position) {},
+              onCameraMove: _manager.onCameraMove,
+              onCameraIdle: _manager.updateMap,
+              onMapCreated: (controller) {
+                _googleMapController = controller;
+                _controller.complete(controller);
+                _manager.setMapId(controller.mapId);
+              },
             ),
           ],
         ));
